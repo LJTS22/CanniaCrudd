889f2b0d717c477949a9d4603ed90955bb7a3ea2
lizeth <lizeth11103@gmail.com>
2025-12-15 16:00:12 -0500

    TerminadoAgregar archivos de proyecto.

---
+++ Models/Empleado.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CanniaCrud.Models
+{
+    public class Empleado
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(100)")]
+        public string Nombre { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(100)")]
+        public string CArgo { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Salario { get; set; }
+    }
+}
+//comentario
